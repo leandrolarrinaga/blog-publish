667f1fdb0a415bb8b68523a5b3f5101a0e8d70b2
GAntun <antun.guillermo@gmail.com>
2021-01-27 16:01:04 -0300

    adding template url to post container query

--- lambda/src/queries.ts
+++ lambda/src/queries.ts
@@ -11,6 +11,7 @@ export const allPostContainersBySiteId = (siteId: string) => `
   postContainerCollection(where:{siteId:{sys:{id:"${siteId}"}}}){
     items{
       blogName
+      templateUrl
     }
   }
 }
